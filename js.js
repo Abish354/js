b74c6151f6c0f4b96d3e2706269bebba1261cc09
Abish354 <abishinamdar2003@gmail.com>
2025-02-25 10:16:01 +0000

    conversion number

---
+++ js.js
@@ -0,0 +1,26 @@
+//conversion Number tro String
+
+let score = 44;
+console.log(typeof score);
+let isNumber = String(score);
+console.log(typeof isNumber);
+console.log = (isNumber);
+
+//convert to srting to number
+
+//let score ="Abish";
+//console.log(score);
+
+//let isString= Number(score);
+//console.log(typeof isString);
+//console.log(isString);
+
+
+
+//"33"= 33
+//"33abc" = NaN =  Not A Number
+
+//true = 1;
+//false = 0;
+
+
